574a3e2fb17b858ce7be779d3ad9a6043fec6807
Yance <yancellymolina12@gmail.com>
2026-03-25 14:41:14 -0500

    Creación y reemplazo de carpetas

--- features/environment.py
+++
@@ -1,8 +0,0 @@
-def before_scenario(context, scenario):
-    context.usuarios = {}
-    context.proyectos = {}
-    context.tareas = {}
-    context.error = None
-    context.proyecto_actual = None
-    context.tarea_actual = None
-    context.tareas_filtradas = []
